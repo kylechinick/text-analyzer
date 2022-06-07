76fe902677dc721b8589040e13ca5cb94f23a916
Kyle Chinick <kylechinick@icloud.com>
2022-06-07 16:35:54 -0700

    Add bold passage function, create utility class and refactor occurances and bold functions, WIP bold function refactor
    Co-authored-by: Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return text.trim().length === 0 || word.trim().length === 0;
+}
+
 // Business Logic
 
 function wordCounter(text) {
@@ -15,7 +21,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0 || word.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(' ');
@@ -45,6 +51,31 @@ function noPottyMouth(text) {
 
 // UI Logic
 
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return '';
+  }
+  let htmlString = '<p>';
+  let textArray = text.split(' ');
+  textArray.forEach(function (element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat('<b>' + element + '</b>');
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== textArray.length - 1) {
+      htmlString = htmlString.concat(' ');
+    }
+  });
+  return htmlString + '</p>';
+}
+
+// WIP solution:
+// let str = "Hello";
+// let substr = "el";
+// str.replace(substr, '<b>' + substr + '</b>');
+// https://stackoverflow.com/questions/29896907/bold-part-of-string
+
 $(document).ready(function () {
   $('form#word-counter').submit(function (event) {
     event.preventDefault();
@@ -54,5 +85,6 @@ $(document).ready(function () {
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $('#total-count').html(wordCount);
     $('#selected-count').html(occurrencesOfWord);
+    $('#bolded-passage').html(boldPassage(word, passage));
   });
 });
